ab56fc2323f8d4cb3e2fd258a98caf5b80ec0efc
Tobias Domhan <tobias@bit.ly>
2013-06-07 13:54:35 -0400

    giphy search

--- giphy_search.py
+++ giphy_search.py
@@ -5,26 +5,19 @@ api_key = "dc6zaTOxFJmzC"
 endpoint = "http://api.giphy.com/v1/gifs/search"
 
 class Giphy:
-    
-    def __init__(self, phrases, api_key):
-    """
-    phrases: A list of phrases for a story.
-    """
+    def __init__(self, api_key=api_key):
+        """
+        phrases: A list of phrases for a story.
+        """
         self._api_key = api_key
-        self._phrases = []
-        params = {"api_key": api_key}    
-        response = requests.get(endpoint, params=params)    
-        data = json.loads(response.content)
 
-    def giphy_search():
-        for item in phrases:
-            item = phrase
-            params = {
+    def giphy_search(self, phrases):
+        for phrase in phrases:
+            query_params = {
                 'api_key': api_key,
                 'q': phrase,
                 'limit': 5
             }
-        
             response = requests.get(endpoint, params=query_params)
 
             data = json.loads(response.content)
@@ -34,7 +27,7 @@ class Giphy:
             for item in data["data"][0:]:
                 original_url = item["images"]["original"]["url"]
                 gif_urls.append(original_url)
-            
+
             return gif_urls
 
 
